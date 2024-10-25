7f04c13b61121ce1a1fe19d1fa5d705f35e837e8
Petrik Diak <diak@petrik.hu>
2024-10-25 16:01:49 +0200

    feat: Complete task

--- src/main/java/hu/petrik/kolcsonzooop/Dvd.java
+++ src/main/java/hu/petrik/kolcsonzooop/Dvd.java
@@ -2,7 +2,7 @@ package hu.petrik.kolcsonzooop;
 
 import java.text.MessageFormat;
 
-public class Dvd implements Kolcsonozheto {
+public class Dvd implements Kolcsonozheto, Comparable<Dvd> {
     private String cim;
     private int hossz;
 
@@ -19,5 +19,23 @@ public class Dvd implements Kolcsonozheto {
     @Override
     public int meddigKolcsonozheto() {
         return 7;
+    }
+
+    @Override
+    public String toString() {
+        return megjelenitendoNev();
+    }
+
+    @Override
+    public int compareTo(Dvd other) {
+        // negatív ha this < other
+        // 0 ha this = other
+        // pozitív ha this > other
+        if (this.hossz == other.hossz) {
+            // Ha egyforma hosszúak, akkor cím szerint abc sorrendben írja ki
+            return this.cim.compareTo(other.cim);
+        }
+        // hossz szerint növekvő sorrend
+        return this.hossz - other.hossz;
     }
 }
--- src/main/java/hu/petrik/kolcsonzooop/Main.java
+++ src/main/java/hu/petrik/kolcsonzooop/Main.java
@@ -1,7 +1,38 @@
 package hu.petrik.kolcsonzooop;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Konyv konyv1 = new Konyv("A Gyűrűk Ura","J. R. R. Tolkien","abc123");
+        Konyv konyv2 = new Konyv("Hobbit","J. R. R. Tolkien","abc321");
+        Konyv konyv3 = new Konyv("Varjak lakomája","George R. R. Martin","aaa111");
+        Dvd dvd1 = new Dvd("Star Wars V. rész – A Birodalom visszavág", 124);
+        Dvd dvd2 = new Dvd("Star Wars IV. rész - Egy új remény", 121);
+        Dvd dvd3 = new Dvd("Star Wars VI. rész - A Jedi visszatér", 131);
+        Dvd dvd4 = new Dvd("A Gyűrűk Ura: A gyűrű szövetsége", 178);
+        Dvd dvd5 = new Dvd("A Gyűrűk Ura: A két torony", 155);
+        Ujsag ujsag1 = new Ujsag("Képes Sport", 2007, 23);
+        Ujsag ujsag2 = new Ujsag("Blikk", 2005, 12);
+
+        List<Kolcsonozheto> katalogus = new ArrayList<>(List.of(konyv1, konyv2, konyv3, dvd1,
+                dvd2, dvd3, dvd4, dvd5, ujsag1, ujsag2));
+
+        for (Kolcsonozheto kolcsonozheto: katalogus) {
+            if (kolcsonozheto instanceof Konyv konyv) {
+                System.out.printf("%s - ", konyv.getIsbn());
+            }
+            System.out.printf("%s - %d\n", kolcsonozheto.megjelenitendoNev(), kolcsonozheto.meddigKolcsonozheto());
+        }
+
+        List<Dvd> dvdk = new ArrayList<>(List.of(dvd1, dvd2, dvd3, dvd4, dvd5));
+        System.out.println(dvdk);
+        Collections.sort(dvdk);
+        System.out.println(dvdk);
+        Collections.sort(dvdk, Collections.reverseOrder());
+        System.out.println(dvdk);
+
     }
 }
